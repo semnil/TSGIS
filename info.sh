3fb39323b7b5c57c6ca2a643f2fc7e883fc076bf
Tetsuya Shinone <info@semnil.com>
2017-01-21 14:33:33 +0900

    Fix a filter for the metacritic url. #3 Closes #11

--- info.sh
+++ info.sh
@@ -124,7 +124,7 @@ fi
 
 
 if [ "$META_LINK" = "" ] ; then
-    META_TITLE=`echo ${DISPLAY_TITLE} | tr "A-Z" "a-z" | sed 's/://g' | sed "s/'//g" | sed 's/_/ /g' | sed 's/ /-/g'`
+    META_TITLE=`echo ${DISPLAY_TITLE} | tr "A-Z" "a-z" | sed -E "s/([:'\?\.&]|-$)//g" | sed 's/[_-]/ /g' | sed -E 's/ +/-/g'`
     # generate a metacritic page URL from title
     URL="http://www.metacritic.com/game/pc/$META_TITLE"
     META_LINK="\"$URL\""
