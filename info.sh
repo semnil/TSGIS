ba5017d64e607a1abbee50cfc2c5faefcd84f284
Tetsuya Shinone <info@semnil.com>
2017-05-03 01:50:22 +0900

    add method and server name to the request url.

--- info.sh
+++ info.sh
@@ -225,7 +225,7 @@ echo "</textarea>"
 tail -n 1 ${HIST_FILE} 2>/dev/null | grep "${DISPLAY_TITLE}" >/dev/null 2>&1
 if [ "${DISPLAY_TITLE}" != "" -a $? -ne 0 ] ; then
     HIST_TITLE=`echo "${DISPLAY_TITLE}" | sed "s/'/%27/g" | sed 's/"/%22/g'`
-    echo -e "${HIST_TITLE}\t${REQUEST_URI}" >> ${HIST_FILE}
+    echo -e "${HIST_TITLE}\thttps://${SERVER_NAME}${REQUEST_URI}" >> ${HIST_FILE}
 fi
 
 output_close_and_exit
