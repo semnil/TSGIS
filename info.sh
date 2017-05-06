1d5d743afa991f32bc78c1f973db8ea4725e39b7
Tetsuya Shinone <info@semnil.com>
2017-05-06 21:14:20 +0900

    Fix metacritic link output with blank result.

--- info.sh
+++ info.sh
@@ -91,7 +91,7 @@ GENRE=`echo ${GENRE} | sed 's/カジュアル//g' | sed 's/独立系開発会社
 REVIEWS=`cat ${TMP_PAGE_FILE} | grep "game_review_summary" | grep "description" | tail -n 1 | sed 's/.*\">//g' | sed 's/<.*//g'`
 echo ${REVIEWS} | grep "user reviews" >/dev/null 2>&1 && REVIEWS=""
 DEVELOPER=`cat ${TMP_PAGE_FILE} | grep "?developer=" | head -n 1 | sed 's/.*\">//g' | sed 's/<.*//g'`
-META_LINK=`cat ${TMP_PAGE_FILE} | grep "game_area_metalink" | sed 's/.*href=//g' | sed 's/ target=.*//g'`
+META_LINK=`cat ${TMP_PAGE_FILE} | grep "game_area_metalink" | grep "href=" | sed 's/.*href=//g' | sed 's/ target=.*//g'`
 
 if [ "`cat ${TMP_PAGE_FILE} | grep -v "bundle_base_discount" | grep 'class="discount_original_price"'`" == "" ] ; then
     # not discounted
