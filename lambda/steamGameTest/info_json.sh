e26d5436e09f9cf651fad97db2b45103a352b564
Tetsuya Shinone <info@semnil.com>
2019-03-17 09:34:02 +0900

    Change the behavior of a function of searching metacritic page.

--- lambda/steamGameTest/info_json.sh
+++ lambda/steamGameTest/info_json.sh
@@ -9,8 +9,8 @@ export LC_MESSAGES='ja_JP.UTF-8'
 TMP_PAGE_FILE=/tmp/tmp.json
 HIST_FILE=/tmp/$(pwd | sed 's/\//./g').hist
 GOOGLE_SEARCH_STR="https://www.googleapis.com/customsearch/v1?key=${GOOGLE_API_KEY}&cx=${GOOGLE_APP_ID}&q=allintitle%3A+steam+%3A+"
-UA_OPTION="User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36"
-METACRITIC_STR="http://www.metacritic.com"
+UA_OPTION="User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36"
+METACRITIC_STR="https://www.metacritic.com"
 
 
 PARAMS=(`echo ${REQUEST_URI} | awk 'BEGIN { FS="?" ; } { print $2 }' | awk 'BEGIN { FS="&" ; } { print $1 }'`)
@@ -165,35 +165,22 @@ fi
 
 
 if [ "${META_LINK}" = "" ] ; then
-    META_TITLE=`echo ${DISPLAY_TITLE} | sed -e 's/&amp;//g' | sed -e 's/’//g' | tr "A-Z" "a-z" | sed -E 's/[^a-zA-Z0-9!-.:-@\[-\`{-~ ]+.*//g' | sed 's/_/ /g' | sed -E 's/ +/-/g' | sed -E "s/([:'\?\.&,\/]|-$|-dx$)//g"`
-    # generate a metacritic page URL from title
-    URL="http://www.metacritic.com/game/pc/${META_TITLE}"
-    META_LINK="\"${URL}\""
-    #echo "<!-- metacritic url = ${URL} -->"
-    #echo "<!-- metacritic page status"
-    STATUS=`curl -L ${URL} -H "${UA_OPTION}" -o ${TMP_PAGE_FILE}  -w '%{http_code}\n' 2>/dev/null`
-    #echo ${STATUS}
-    #echo "-->"
-
-    if [ "${STATUS}" != "200" ] ; then
-        # search a metacritic page
-        QUERY=`echo ${DISPLAY_TITLE} | sed -e 's/&amp;//g' | tr "A-Z" "a-z" | sed -E 's/- .* -$//g' | sed -E 's/:.*$//g' | sed -E 's/[^a-zA-Z0-9!-.:-@¥[-\`{-~ ]+.*//g' | sed 's/_/ /g' | sed -E 's/ +/-/g' | sed -E "s/([:\?\.&,\/]|-$|-dx$)//g" | sed -E 's/-+/+/g'`
-        URL=`echo ${METACRITIC_STR}/search/game/${QUERY}/results | sed -E 's/\++/%20/g'`
-        #echo "<!-- metacritic search query = ${QUERY} -->"
-        curl "${URL}" -XPOST -H "${UA_OPTION}" --data "search_term=${QUERY}&search_filter=game" 2>/dev/null > ${TMP_PAGE_FILE}
-        RESULT=`cat ${TMP_PAGE_FILE} | grep "product_title basic_stat" | head -n 1 | sed 's/^.* href="//g' | sed 's/".*$//g'`
-        if [ "${RESULT}" != "" ] ; then
-            URL=${METACRITIC_STR}${RESULT}
-            META_LINK="\"${URL}\""
-            #echo "<!-- metacritic url = ${URL} -->"
-            #echo "<!-- metacritic page status"
-            STATUS=`curl -L ${URL} -H "${UA_OPTION}" -o ${TMP_PAGE_FILE}  -w '%{http_code}\n' 2>/dev/null`
-            #echo ${STATUS}
-            #echo "-->"
-        else
-            META_LINK=""
-            METASCORE_STR="-"
-        fi
+    # search a metacritic page
+    QUERY=`echo ${DISPLAY_TITLE} | sed -e 's/&amp;//g' | tr "A-Z" "a-z" | sed -E 's/- .* -$//g' | sed -E 's/:.*$//g' | sed -E 's/[^a-zA-Z0-9!-.:-@¥[-\`{-~ ]+.*//g' | sed 's/_/ /g' | sed -E 's/ +/-/g' | sed -E "s/([:\?\.&,\/]|-$|-dx$)//g" | sed -E 's/-+/+/g' | sed -E 's/\++/%20/g'`
+    URL=`echo ${METACRITIC_STR}/search/game/${QUERY}/results\?plats\\\[3\\\]=1\&search_type=advanced`
+    #echo "<!-- metacritic search query = ${QUERY} -->"
+    curl "${URL}" -H "${UA_OPTION}" 2>/dev/null > ${TMP_PAGE_FILE}
+    RESULT=`cat ${TMP_PAGE_FILE} | grep "href=\"/game/pc/" | head -n 1 | sed 's/^.* href="//g' | sed 's/".*$//g'`
+    if [ "${RESULT}" != "" ] ; then
+        URL=${METACRITIC_STR}${RESULT}
+        META_LINK="\"${URL}\""
+        #echo "<!-- metacritic url = ${URL} -->"
+        #echo "<!-- metacritic page status"
+        STATUS=`curl -L ${URL} -H "${UA_OPTION}" -o ${TMP_PAGE_FILE}  -w '%{http_code}\n' 2>/dev/null`
+        #echo ${STATUS}
+        #echo "-->"
+    else
+        METASCORE_STR="-"
     fi
 else
     URL=`echo ${META_LINK} | awk 'BEGIN { FS="\""; } { print $2 }'`
