acb025c7d9f17b2921072691498255b68a04504e
Tetsuya Shinone <info@semnil.com>
2019-11-10 15:18:31 +0900

    Fix a rule for the low price

--- lambda/steamGame/info_json.sh
+++ lambda/steamGame/info_json.sh
@@ -135,9 +135,9 @@ DB_LINK="\"https://steamdb.info/app/${APP_ID}/?cc=jp\""
 #echo "<!-- steamdb url = ${DB_LINK} -->"
 curl -b cc=jp -L "https://steamdb.info/app/${APP_ID}/" 2>/dev/null > ${TMP_PAGE_FILE}
 PRICE_BLOCK=`cat -n ${TMP_PAGE_FILE} | grep 'class="price-line"' | grep 'data-cc="jp"' | awk '{ print $1 }'`
-PRICE_LINE=`expr ${PRICE_BLOCK} + 7`
+PRICE_LINE=`expr ${PRICE_BLOCK} + 6`
 if [ "${PRICE_BLOCK}" != "" -a "${PRICE_LINE}" != "" ] ; then
-    LOW_PRICE=`cat ${TMP_PAGE_FILE} | tail -n+${PRICE_LINE} | head -n 1 | sed 's/.*">¥ *//g' | sed 's/ *at.*//g' | sed 's/<\/.*//g'`
+    LOW_PRICE=`cat ${TMP_PAGE_FILE} | tail -n+${PRICE_LINE} | head -n 1 | sed -e 's/<[^>]*>[^0-9]*//g' | sed 's/ *at.*//g' | sed 's/<\/.*//g'`
     if [ "${LOW_PRICE}" = "" ] ; then
         #echo "<!-- get USD price -->"
         PRICE_BLOCK=`cat -n ${TMP_PAGE_FILE} | grep 'class="price-line"' | grep 'data-cc="us"' | awk '{ print $1 }'`
