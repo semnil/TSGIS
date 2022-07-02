589c5ae65a4c783d8c88ae5e712de1ef4f7f3375
Tetsuya Shinone <1051877+semnil@users.noreply.github.com>
2022-07-02 11:08:49 +0900

    Fix env value

--- lambda/build.sh
+++ lambda/build.sh
@@ -4,7 +4,7 @@ cd `dirname $0`
 #source .env
 
 cat swagger-template.yaml | sed "s/account_placeholder/${AWS_ACCOUNT}/g" | sed "s/region_placeholder/${AWS_REGION}/g" > swagger.yaml
-cat sam-base.yaml | sed "s/bucket_placeholder/${AWS_BUCKET}/g" | sed "s/table_name_placeholder/${TABLE_NAME}/g" | sed "s/role_arn_placeholder/${LAMBDA_ROLE_ARN}/g" | sed "s/encrypted_google_api_key_placeholder/${ENCRYPTED_GOOGLE_API_KEY}/g" | sed "s/encrypted_google_app_id_placeholder/${ENCRYPTED_GOOGLE_APP_ID}/g" | sed "s/google_search_key_placeholder/${GOOGLE_SEARCH_KEY}/g" | sed "s/kms_key_arn_placeholder/${KMS_KEY_ARN}/g" > sam-template.yaml
+cat sam-base.yaml | sed "s/bucket_placeholder/${AWS_BUCKET}/g" | sed "s/table_name_placeholder/${TABLE_NAME}/g" | sed "s/role_arn_placeholder/${LAMBDA_ROLE_ARN}/g" | sed "s/encrypted_google_api_key_placeholder/${ENCRYPTED_GOOGLE_API_KEY}/g" | sed "s/encrypted_google_app_id_placeholder/${ENCRYPTED_GOOGLE_APP_ID}/g" | sed "s/google_search_key_placeholder/'${GOOGLE_SEARCH_KEY}'/g" | sed "s/kms_key_arn_placeholder/${KMS_KEY_ARN}/g" > sam-template.yaml
 
 aws cloudformation package --template-file sam-template.yaml --output-template-file ../sam-output.yaml --s3-bucket ${AWS_BUCKET} --s3-prefix lambda
 
